f7824224ea87b58b825241d0b597330e3a9db6f1
Matthew Ricci <s3785111@student.rmit.edu.au>
2024-04-09 18:08:13 +1000

    Adjustments to sync logic

--- avionics/tests/Integration/Integration.ino
+++ avionics/tests/Integration/Integration.ino
@@ -6,10 +6,13 @@
 
 // Define the chip select
 #define CSPIN 10
+
+// Define terminal pin connections
 #define TERMINAL_4 4  // Blue Raven takeoff
 #define TERMINAL_M 5  // Blue Raven apogee
 
 double groundpressure;
+int flagTakeoff = 0;
 
 void setup() {
   // i2c at 400kHz
@@ -40,11 +43,12 @@ uint32_t pageAddr = 0;  // Starting page address
 // Timers for high and low res
 unsigned long previousHighResolutionMicro = 0;
 unsigned long previousLowResolutionMicro = 0;
-const unsigned long highResolutionInterval = 2000;  // Interval for high resolution (500gz) in microseconds (1000000 microseconds / 500gz)
-const unsigned long lowResolutionInterval = 20000;  // Interval for low resolution (50gz) in microseconds (1000000 microseconds / 50gz)
+// const unsigned long highResolutionInterval = 2000;  // Interval for high resolution (500Hz) in microseconds (1000000 microseconds / 500Hz)
+const unsigned long highResolutionInterval = 4000;  // Interval for high resolution (250Hz) in microseconds (1000000 microseconds / 250Hz)
+const unsigned long lowResolutionInterval = 20000;  // Interval for low resolution (50Hz) in microseconds (1000000 microseconds / 50Hz)
 
 // Timer for Sync
-unsigned long previousMicros = 0;
+uint8_t sync = 0;  
 const unsigned long syncMicros = 249000;  // 249ms interval in microseconds
 
 // Buffer size for storing sensor data
@@ -52,19 +56,20 @@ const size_t BUFFER_SIZE = 256;   // 256 byte per pages
 uint8_t dataBuffer[BUFFER_SIZE];  // Buffer to store sensor data
 uint8_t bufferIndex = 0;
 
-
 void loop() {
-  // Current time in microseconds
-  unsigned long currentMicro = micros();
 
-  // Check if 249ms has elapsed for sync
-  if (currentMicro - previousMicros >= syncMicros) {
-    // Reset the timer
-    previousMicros = currentMicro;
-  }
+  // Current time in microseconds
+  unsigned long currentHighResMicro = micros();
 
   // High resolution loop (500Hz)
-  if (currentMicro - previousHighResolutionMicro >= highResolutionInterval) {
+  if (currentHighResMicro - previousHighResolutionMicro >= highResolutionInterval) {
+
+    // Check if 249ms has elapsed for sync
+    sync = currentHighResMicro - previousHighResolutionMicro;
+    if (sync >= syncMicros) {
+      // Reset the timer
+      sync = 0;
+    }
 
     // Accel ---------------------------------------------------------------------------------------
     int16_t accel[3];
@@ -98,11 +103,11 @@ void loop() {
     sprintf(str, "Mag: x=%d y=%d z=%d", mx, my, mz);
     Serial.println(str);
 
-    // convert currentMicro from mirco to millis
-    uint8_t syncMillis = currentMicro / 1000;  // Convert microseconds to milliseconds and truncate
+    uint8_t syncMillis = sync / 1000;  // Convert microseconds to milliseconds and truncate
 
     // Create Buffer
     dataBuffer[bufferIndex++] = 0b01010100;  // 01 010100 (ID and length)
+
     dataBuffer[bufferIndex++] = syncMillis;
 
     dataBuffer[bufferIndex++] = (uint8_t)((ax >> 8) & 0xFF);  // Store the high byte of ax
@@ -127,11 +132,20 @@ void loop() {
     dataBuffer[bufferIndex++] = (uint8_t)(mz & 0xFF);         // Store the low byte of mz
 
     // reset high res timer
-    previousHighResolutionMicro = currentMicro;
+    previousHighResolutionMicro = currentHighResMicro;
   }
 
   // Low resolution loop (50Hz)
-  if (currentMicro - previousLowResolutionMicro >= lowResolutionInterval) {
+  unsigned long currentLowResMicro = micros();
+
+  if (currentLowResMicro - previousLowResolutionMicro >= lowResolutionInterval) {
+
+    // Check if 249ms has elapsed for sync
+    sync = currentHighResMicro - previousHighResolutionMicro;
+    if (sync >= syncMicros) {
+      // Reset the timer
+      sync = 0;
+    }
 
     //Barometer ---------------------------------------------------------------------------------------------------
     int32_t baro[2];
@@ -142,7 +156,7 @@ void loop() {
     Serial.println(str);
 
     // convert currentMicro from mirco to millis
-    uint8_t syncMillis = currentMicro / 1000;  // Convert microseconds to milliseconds and truncate
+    uint8_t syncMillis = sync-previousLowResolutionMicro / 1000;  // Convert microseconds to milliseconds and truncate
 
     // Shift to 3 bytes and add to Buffer
     dataBuffer[bufferIndex++] = 0b10001000;  // 10 001000 (ID and length)
@@ -157,12 +171,16 @@ void loop() {
     dataBuffer[bufferIndex++] = (uint8_t)(baro[1] & 0xFF);          // Store the third byte of temperature
 
     // Write to flash
+    Serial.println("Waiting for flash...");
     while (!SerialFlash.ready())
       ;
+    Serial.println("Flash ready.");
     SerialFlash.write(pageAddr, dataBuffer, sizeof(dataBuffer));
+    Serial.println("Page successfully written to flash.");
+    Serial.println("");
 
     pageAddr += 0x100;
     bufferIndex = 0;
-    previousLowResolutionMicro = currentMicro;
+    previousLowResolutionMicro = currentLowResMicro;
   }
 }
